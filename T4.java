c4c32430bce3307af323aa7e51fb87731cdca1e5
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2024-09-30 21:02:40 +0530

    Create T4.java

---
+++ T4.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        double decimal=7.28;
+        int num=(int)decimal;
+        System.out.println("Integer value:"+num);
+    }
+}
