09662f5c49dedf1f707b55ee694121bfef9a07e6
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2024-09-30 20:58:18 +0530

    Create T3.java

---
+++ T3.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        String greeting="Hello";
+        String name="Khushi";
+        String message=greeting+","+name+"!";
+        System.out.println(message);
+    }
+}
